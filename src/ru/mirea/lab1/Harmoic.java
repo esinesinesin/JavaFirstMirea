8fbf32dcf1777369c71cd9e67848867eebe31361
EsinIS <esinisesinis@gmail.com>
2022-09-20 10:41:19 +0300

    Практическая 1.4

---
+++ src/ru/mirea/lab1/Harmoic.java
@@ -0,0 +1,13 @@
+package ru.mirea.lab1;
+
+public class Harmoic {
+    public static void main(String[] args) {
+        double ans = 0.0;
+        for (int i = 1; i < 11; i++) {
+            double x = 1.0 / i;
+            ans += x;
+            System.out.printf("%.2f ",x);
+        }
+        System.out.printf("%.2f ", ans);
+    }
+}
